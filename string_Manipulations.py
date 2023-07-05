7952835119ccd86ba119e65336b7bb0ea6a0c23d
xinxiongdi <97471993+tsxing@users.noreply.github.com>
2023-07-05 11:52:25 -0400

    Update string_Manipulations.py

--- string_Manipulations.py
+++ string_Manipulations.py
@@ -5,3 +5,7 @@ s = s[:(len(str)-1)//2] #odd
 #remove duplicates from string
 s ="heellloooh"
 print("".join(dict.fromkeys(s)))  #outputs "helo"
+
+#make comma separated string to list 
+s = "hello, apple, orange"
+x = s.split(", ")
